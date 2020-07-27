4bc573cf8bbe326e0a98ab975aae53cd40854066
Jagbai <33807790+Jagbai@users.noreply.github.com>
2020-07-27 18:27:51 +0100

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/Jagbai/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Jagbai')
+            node / gitConfigEmail('james.agbai@hotmail.com')
         }
     }
     triggers {
@@ -13,6 +13,6 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        bat("npm install")
+        batchFile("npm install")
     }
 }
